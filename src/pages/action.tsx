049ded604a4ac713b8afd74d1442f453559dc583
Tippe <tippevanroosmalen@outlook.com>
2025-08-15 15:35:08 +0200

    ChatDrawer gemaakt naar v6, action pagina een WIP toegevoegd

--- src/pages/action.tsx
+++ src/pages/action.tsx
@@ -1,6 +1,29 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
+import { Engineering, Settings } from "@mui/icons-material";
+import { Stack } from "@mui/material";
 
 export default function ActionPage() {
-    return <Typography>Welcome to Toolpad!</Typography>;
+    return (
+        <Stack
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+            }}
+        >
+            <Settings
+                sx={{
+                    fontSize: 450,
+                    animation: "spin 20s linear infinite",
+                    "@keyframes spin": {
+                        "0%": { transform: "rotate(0deg)" },
+                        "100%": { transform: "rotate(360deg)" },
+                    },
+                }}
+            />
+            <Typography variant="h2">Work in Progress</Typography>
+        </Stack>
+    );
 }
